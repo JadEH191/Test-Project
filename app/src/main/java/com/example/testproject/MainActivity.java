cc8e84dcac2c59ba904ef5e4db5798d2ee32b041
David <forpersonalonly191@gmail.com>
2023-03-22 22:23:04 +0800

    david

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+    public void btnPopUp(View view){
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View viewpopup = layoutInflater.inflate(R.layout.newtodo, null);
+
+        PopupWindow popupWindow = new PopupWindow(viewpopup, 1000, 500, true);
+
+        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
     }
 }
